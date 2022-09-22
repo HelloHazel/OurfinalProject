a0f31d60a91163b20ed01a16c93d7d5f3a3753d3
yeonhee <yeonhee12134@gmail.com>
2022-09-22 13:46:13 +0900

    MemberDTO 수정

--- src/main/java/com/example/finalproject/model/member/dto/MemberDTO.java
+++ src/main/java/com/example/finalproject/model/member/dto/MemberDTO.java
@@ -1,5 +1,77 @@
 package com.example.finalproject.model.member.dto;
 
-public class MemberDTO {
+import java.util.Date;
 
+public class MemberDTO {
+	private String userid;
+	private String passwd;
+	private String name;
+	private String email;
+	private String phone;
+	private Date join_date;
+	private String zipcode;
+	private String address1; //주소
+	private String address2; //상세주소
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Date getJoin_date() {
+		return join_date;
+	}
+	public void setJoin_date(Date join_date) {
+		this.join_date = join_date;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [userid=" + userid + ", passwd=" + passwd + ", name=" + name + ", email=" + email + ", phone="
+				+ phone + ", join_date=" + join_date + ", zipcode=" + zipcode + ", address1=" + address1 + ", address2="
+				+ address2 + "]";
+	}
+	
+	
 }
