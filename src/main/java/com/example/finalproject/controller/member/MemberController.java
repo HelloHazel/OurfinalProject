3e98ada9dbd376c01c85ea4ad3e5ebf1975e096d
yeonhee <yeonhee12134@gmail.com>
2022-09-22 12:03:21 +0900

    테스트

--- src/main/java/com/example/finalproject/controller/member/MemberController.java
+++ src/main/java/com/example/finalproject/controller/member/MemberController.java
@@ -1,5 +1,10 @@
 package com.example.finalproject.controller.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.finalproject.HomeController;
+
 public class MemberController {
- ㅁㄴㅇㄹㅁㄴㅇㄹ
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 }
