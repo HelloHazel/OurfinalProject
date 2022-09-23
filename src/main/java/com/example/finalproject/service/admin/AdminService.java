a5d7bcceafbdf3d322d289ec871ab437d061aa2e
jayeon Son <sjyun0000@naver.com>
2022-09-23 10:53:02 +0900

    admin관련 commit

---
+++ src/main/java/com/example/finalproject/service/admin/AdminService.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.service.admin;
+
+import com.example.finalproject.model.member.dto.MemberDTO;
+
+public interface AdminService {
+		public String loginCheck(MemberDTO dto);
+}
