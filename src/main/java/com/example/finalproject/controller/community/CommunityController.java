5232cf9df2e4b6feec253a2dd135a71ff48ba2fd
User <User@DESKTOP-KJ7MLQ3>
2022-09-27 20:35:29 +0900

    -community 링크 연결 및 리스트 불러오기

--- src/main/java/com/example/finalproject/controller/community/CommunityController.java
+++ src/main/java/com/example/finalproject/controller/community/CommunityController.java
@@ -1,5 +1,18 @@
 package com.example.finalproject.controller.community;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("community/*")
 public class CommunityController {
+	
+
+	@RequestMapping("list.do")
+	public String list(Model model) {
+		model.addAttribute("message","홈페이지 방문을 환영합니다.");
+		return "community/post_list";
+	}
 
 }
